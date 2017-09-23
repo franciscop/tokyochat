35ad3d8c7cf5d5ad1e3179965d2f682ebf70807a
Francisco Presencia <public@francisco.io>
2017-09-23 20:20:19 +0900

    Fixed basic chat

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const server = require('server');
 const { get, socket } = server.router;
 const { render } = server.reply;
 
-server({ views: 'public' },
-  get('/', ctx => {
-    return render('index.html');
-  }),
-  socket('connect', ctx => {
-    console.log('Connected:', ctx.socket.id);
-  }),
-  socket('message', ctx => {
-    console.log('Message:', ctx.data);
-    ctx.io.emit('message', ctx.data)
-  }),
-  socket('disconnect', ctx => {
-    console.log('Disconnected:', ctx.socket.id);
-  })
-);
+// Update everyone with the current user count
+const updateCounter = ctx => {
+  ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+};
+
+// Send the new message to everyone
+const sendMessage = ctx => {
+  ctx.io.emit('message', ctx.data);
+};
+
+server([
+  get('/', ctx => render('index.html')),
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter),
+  socket('message', sendMessage)
+]);
