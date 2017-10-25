b35b3e5df5dcee38aa0c6b2f76407543a2c796e8
Francisco Presencia <public@francisco.io>
2017-10-26 00:05:26 +0900

    Fixed cookies

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const server = require('server');
 const { get, socket } = server.router;
 const { render } = server.reply;
 
-// Update everyone with the current user count
 const updateCounter = ctx => {
   ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
 };
 
-// Send the new message to everyone
-const sendMessage = ctx => {
-  ctx.io.emit('message', ctx.data);
-};
-
-server([
+server({ port: 3001 }, [
   get('/', ctx => render('index.html')),
   socket('connect', updateCounter),
   socket('disconnect', updateCounter),
-  socket('message', sendMessage)
+  socket('message', ctx => {
+    console.log(ctx.data);
+    ctx.io.emit('message', ctx.data);
+  })
 ]);
---
+++ public/javascript.js
@@ -0,0 +1,47 @@
+// ./public/javascript.js
+
+// Get the current username from the cookies
+var user = cookie.get('user');
+if (!user) {
+
+  // Ask for the username if there is none set already
+  user = prompt('Choose a username:');
+  if (!user) {
+    alert('We cannot work with you like that!');
+  } else {
+    // Store it in the cookies for future use
+    cookie.set('user', user);
+  }
+}
+
+var socket = io();
+
+// The user count. Can change when someone joins/leaves
+socket.on('count', function (data) {
+  $('.user-count').html(data);
+});
+
+// When we receive a message
+// it will be like { user: 'username', message: 'text' }
+socket.on('message', function (data) {
+  $('.chat').append('<p><strong>' + data.user + '</strong>: ' + data.message + '</p>');
+});
+
+// When the form is submitted
+$('form').submit(function (e) {
+  // Avoid submitting it through HTTP
+  e.preventDefault();
+
+  // Retrieve the message from the user
+  var message = $(e.target).find('input').val();
+
+  // Send the message to the server
+  socket.emit('message', {
+    user: cookie.get('user') || 'Anonymous',
+    message: message
+  });
+
+  // Clear the input and focus it for a new message
+  e.target.reset();
+  $(e.target).find('input').focus();
+});
